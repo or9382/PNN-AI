8e01c10530253992ecb13183fd2922b0d779c766
Or Borenstein <borenstein@campus.technion.ac.il>
2019-08-26 18:51:24 +0300

    changed label to integers

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -2,5 +2,7 @@
 from .lwir import LWIR
 from .vir import *
 from .modalities import Modalities
+from .labels import classes
 
-__all__ = ['LWIR', 'VIR577nm', 'VIR692nm', 'VIR732nm', 'VIR970nm', 'VIRPolar', 'Modalities']
+__all__ = ['LWIR', 'VIR577nm', 'VIR692nm', 'VIR732nm', 'VIR970nm', 'VIRPolar',
+           'Modalities', 'classes']
