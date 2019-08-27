5861da2a1a97e9db7fabeda9f68cc460e0b28c91
Or Borenstein <borenstein@campus.technion.ac.il>
2019-08-27 12:54:56 +0300

    added date parameters to the training loop

--- train/train_loop.py
+++ train/train_loop.py
@@ -1,4 +1,5 @@
 
+from datetime import datetime
 import torch
 from torch import nn, optim
 from torch.utils import data
@@ -11,29 +12,34 @@ label_lr = 0.001
 plant_lr = 0.001
 extractor_lr = 0.001
 
+start_date = datetime(2019, 6, 5)
+end_date = datetime(2019, 6, 23)
+lwir_max_len = 250
+vir_max_len = 16
+
 domain_adapt_l = 0.01
 
 modalities = {
-    '577nm': {'max_len': 36},
-    'polar': {'max_len': 36}
+    '577nm': {'max_len': vir_max_len},
+    'polar': {'max_len': vir_max_len}
 }
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-dataset = Modalities('Exp0', split_cycle=7, **modalities)
+dataset = Modalities(
+    'Exp0', split_cycle=7, start_date=start_date, end_date=end_date, **modalities
+)
 dataloader = data.DataLoader(dataset, batch_size=4, num_workers=2)
 
 feat_ext = FeatureExtractor(*modalities)
 label_cls = nn.Sequential(nn.Linear(512, len(classes)), nn.Softmax())
 plant_cls = nn.Sequential(nn.Linear(512, 48), nn.Softmax())
 
-plant_cls.register_backward_hook(lambda grad: -domain_adapt_l * grad)
-
 criterion = nn.CrossEntropyLoss()
 
-label_opt = optim.adam.Adam(label_cls.parameters(), lr=label_lr)
-plant_opt = optim.adam.Adam(plant_cls.parameters(), lr=plant_lr)
-ext_opt = optim.adam.Adam(feat_ext.parameters(), lr=extractor_lr)
+label_opt = optim.Adam(label_cls.parameters(), lr=label_lr)
+plant_opt = optim.Adam(plant_cls.parameters(), lr=plant_lr)
+ext_opt = optim.Adam(feat_ext.parameters(), lr=extractor_lr)
 
 for epoch in range(epochs):
     print(f"epoch {epoch + 1} - ", end='')
@@ -60,11 +66,10 @@ for epoch in range(epochs):
 
         plant_out = plant_cls(features)
         plant_loss = criterion(plant_out, plants)
-        plant_loss.backward()
+        (-plant_loss).backward()
 
         label_opt.step()
         ext_opt.step()
         plant_opt.step()
-
 
     print(f"loss: {running_loss}")
