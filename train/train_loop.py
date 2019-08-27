2cc9e19ab959da0ac2a2d1431d67bd60a8b82c35
Or Borenstein <borenstein@campus.technion.ac.il>
2019-08-27 16:23:39 +0300

    edited train loop's modalities in use and hyper-parameters

--- train/train_loop.py
+++ train/train_loop.py
@@ -7,26 +7,31 @@ from datasets import Modalities, classes
 from model import LinearWrapper, PlantFeatureExtractor as FeatureExtractor
 
 epochs = 5
-label_lr = 0.001
-plant_lr = 0.001
+label_lr = 0.01
+plant_lr = 0.01
 extractor_lr = 0.001
 
 start_date = datetime(2019, 6, 5)
 end_date = datetime(2019, 6, 23)
+split_cycle = 7
 lwir_max_len = 250
 vir_max_len = 16
 
-domain_adapt_l = 0.01
+domain_adapt_l = 0.1
 
 modalities = {
+    'lwir': {'max_len': lwir_max_len},
     '577nm': {'max_len': vir_max_len},
+    '692nm': {'max_len': vir_max_len},
+    '732nm': {'max_len': vir_max_len},
+    '970nm': {'max_len': vir_max_len},
     'polar': {'max_len': vir_max_len}
 }
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 dataset = Modalities(
-    'Exp0', split_cycle=7, start_date=start_date, end_date=end_date, **modalities
+    'Exp0', split_cycle=split_cycle, start_date=start_date, end_date=end_date, **modalities
 )
 dataloader = data.DataLoader(dataset, batch_size=4, num_workers=2)
 
@@ -40,12 +45,13 @@ label_opt = optim.Adam(label_cls.parameters(), lr=label_lr)
 plant_opt = optim.Adam(plant_cls.parameters(), lr=plant_lr)
 ext_opt = optim.Adam(feat_ext.parameters(), lr=extractor_lr)
 
+best_loss = float('inf')
 for epoch in range(epochs):
     print(f"epoch {epoch + 1} - ", end='')
 
-    running_loss = 0.
+    tot_label_loss = 0.
+    tot_plant_loss = 0.
     for i, batch in enumerate(dataloader):
-        print(i)
 
         labels = batch['label']
         plants = batch['plant']
@@ -72,4 +78,23 @@ for epoch in range(epochs):
         ext_opt.step()
         plant_opt.step()
 
-    print(f"loss: {running_loss}")
+        tot_label_loss += label_loss.item()
+        tot_plant_loss += plant_loss.item()
+
+        if i % 6 == 5:
+            print(f"{i}. label loss: {tot_label_loss / 6}")
+            print(f"{i}. plant loss: {tot_plant_loss / 6}")
+
+            if tot_label_loss < best_loss:
+                torch.save({
+                    'epoch': epoch,
+                    'feat_ext_state_dict': feat_ext.state_dict(),
+                    'label_cls_state_dict': label_cls.state_dict(),
+                    'plant_cls_state_dict': plant_cls.state_dict(),
+                    'loss': tot_label_loss
+                }, f'checkpoint')
+
+                best_loss = tot_label_loss
+
+            tot_label_loss = 0.
+            tot_plant_loss = 0.
