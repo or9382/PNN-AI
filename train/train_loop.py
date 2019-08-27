d034ba5c5ebc60db2ee6c1ba8b25190b3cd188af
Assaf-Haik-Barouch <Assaf98@gmail.com>
2019-08-27 12:04:34 +0300

    fixed training loop

--- train/train_loop.py
+++ train/train_loop.py
@@ -9,6 +9,10 @@ from model import LinearWrapper, PlantFeatureExtractor as FeatureExtractor
 epochs = 5
 label_lr = 0.001
 plant_lr = 0.001
+extractor_lr = 0.001
+
+domain_adapt_l = 0.01
+
 modalities = {
     '577nm': {'max_len': 36},
     'polar': {'max_len': 36}
@@ -20,12 +24,16 @@ dataset = Modalities('Exp0', split_cycle=7, **modalities)
 dataloader = data.DataLoader(dataset, batch_size=4, num_workers=2)
 
 feat_ext = FeatureExtractor(*modalities)
-label_cls = LinearWrapper(feat_ext, 512, len(classes))
-plant_cls = LinearWrapper(feat_ext, 512, 48)
+label_cls = nn.Sequential(nn.Linear(512, len(classes)), nn.Softmax())
+plant_cls = nn.Sequential(nn.Linear(512, 48), nn.Softmax())
+
+plant_cls.register_backward_hook(lambda grad: -domain_adapt_l * grad)
 
 criterion = nn.CrossEntropyLoss()
-label_opt = optim.SGD(label_cls.parameters(), lr=label_lr)
-plant_opt = optim.SGD(plant_cls.parameters(), lr=plant_lr)
+
+label_opt = optim.adam.Adam(label_cls.parameters(), lr=label_lr)
+plant_opt = optim.adam.Adam(plant_cls.parameters(), lr=plant_lr)
+ext_opt = optim.adam.Adam(plant_cls.parameters(), lr=extractor_lr)
 
 for epoch in range(epochs):
     print(f"epoch {epoch + 1} - ", end='')
@@ -42,15 +50,21 @@ for epoch in range(epochs):
 
         label_opt.zero_grad()
         plant_opt.zero_grad()
+        ext_opt.zero_grad()
 
-        label_out = label_cls(**batch)
+        features = feat_ext(**batch)
+
+        label_out = label_cls(features)
         label_loss = criterion(label_out, labels)
         label_loss.backward()
-        label_opt.step()
 
-        plant_out = plant_cls(**batch)
+        plant_out = plant_cls(features)
         plant_loss = criterion(plant_out, plants)
-        (-plant_loss).backward()
+        plant_loss.backward()
+
+        label_opt.step()
+        ext_opt.step()
         plant_opt.step()
+
 
     print(f"loss: {running_loss}")
