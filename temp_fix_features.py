156652a259de5e5d1cac7b3b42e38053e2053a55
Assaf-Haik-Barouch <Assaf98@gmail.com>
2019-10-14 10:08:37 +0300

    fixed temp script

--- temp_fix_features.py
+++ temp_fix_features.py
@@ -1,16 +1,24 @@
 import pandas as pd
 from torch.utils import data
+from math import ceil
 
 from train.train_loop import dataset
+from datasets import labels
 
-if __name__ == '__main__':
+
+def add_plant_ids():
     df = pd.read_csv('features.csv')
 
     df.to_csv('features_backup.csv', index=False)
 
-    dataloader = data.DataLoader(dataset, batch_size=4, num_workers=4)
-    plants_info = pd.Series().append([pd.Series(data=batch['plant']) for batch in dataloader])
+    n_samples = len(df)
+    n_plants = len(labels)
+    plants_info = pd.Series(data=sum([list(range(n_plants)) for _ in range(ceil(n_samples / n_plants))])[:n_samples])
 
     df['plant'] = plants_info
 
     df.to_csv('features.csv', index=False)
+
+
+if __name__ == '__main__':
+    add_plant_ids()
