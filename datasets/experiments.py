c3d159be00d3f9f97902f21125852b267421e9ad
Assaf-Haik-Barouch <Assaf98@gmail.com>
2019-10-19 13:27:09 +0300

    changed transforms to match the new outputs. removed normalisation to check if the inception normalisation works alone also added a modalities field to ExpInfo

--- datasets/experiments.py
+++ datasets/experiments.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from typing import NamedTuple, Dict, Tuple, List, Any
+from typing import NamedTuple, Dict, Tuple, List
 import torchvision.transforms as T
 from .transformations import RandomCrop, RandomHorizontalFlip, RandomVerticalFlip
 
@@ -13,15 +13,14 @@ class ExpPositions:
 class ExpInfo(NamedTuple):
     start_date: datetime
     end_date: datetime
-    modalities_norms: Dict[str, Tuple[List[float], List[float]]]
+    modalities: Tuple[str, ...]
 
 
 def get_experiment_modalities(exp_info: ExpInfo, lwir_skip: int, lwir_max_len: int, vir_max_len: int):
     modalities: Dict[str, Dict] = {
         'lwir': {
             'max_len': lwir_max_len, 'skip': lwir_skip, 'transform': T.Compose(
-                [T.Normalize(*exp_info.modalities_norms['lwir']), T.ToPILImage(),
-                 RandomCrop((229, 229)), RandomHorizontalFlip(),
+                [RandomCrop((229, 229)), RandomHorizontalFlip(),
                  RandomVerticalFlip(), T.ToTensor()])
         }
     }
@@ -30,10 +29,9 @@ def get_experiment_modalities(exp_info: ExpInfo, lwir_skip: int, lwir_max_len: i
         {
             mod: {
                 'max_len': vir_max_len, 'transform': T.Compose(
-                    [T.Normalize(*norms), T.ToPILImage(),
-                     RandomCrop((458, 458)), RandomHorizontalFlip(),
+                    [RandomCrop((458, 458)), RandomHorizontalFlip(),
                      RandomVerticalFlip(), T.ToTensor()])
-            } for mod, norms in exp_info.modalities_norms.items() if mod != 'lwir'
+            } for mod, norms in exp_info.modalities if mod != 'lwir'
         }
     )
 
@@ -44,14 +42,7 @@ experiments_info: Dict[str, ExpInfo] = {
     'Exp0': ExpInfo(
         datetime(2019, 6, 5),
         datetime(2019, 6, 19),
-        {
-            'lwir': ([21361.], [481.]),
-            '577nm': ([.00607], [.00773]),
-            '692nm': ([.02629], [.04364]),
-            '732nm': ([.01072], [.11680]),
-            '970nm': ([.00125], [.00095]),
-            'polar': ([.05136], [.22331]),
-        }
+        ("577nm", "692nm", "732nm", "970nm", "lwir", "polar")
     ),
 }
 
