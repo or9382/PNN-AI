936e4e63ba753fed9a36fb2766b1c0debaac1eac
Assaf-Haik-Barouch <Assaf98@gmail.com>
2019-10-20 10:16:38 +0300

    changed function name

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -2,7 +2,7 @@ from .lwir import LWIR
 from .vir import *
 from .modalities import Modalities, ModalitiesSubset
 from .labels import classes
-from .experiments import get_experiment_modalities, experiments_info, ExpInfo
+from .experiments import get_experiment_modalities_params, experiments_info, ExpInfo
 
 __all__ = ['LWIR', 'VIR577nm', 'VIR692nm', 'VIR732nm', 'VIR970nm', 'VIRPolar', 'Modalities', 'ModalitiesSubset',
-           'classes', 'get_experiment_modalities', 'experiments_info', 'ExpInfo']
+           'classes', 'get_experiment_modalities_params', 'experiments_info', 'ExpInfo']
